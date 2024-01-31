8ee80059d8c02cf2f8197061e0fd1c40452e160f
Kaua Librelato da Costa <kaualibrelatodacosta@gmail.com>
2024-01-31 16:56:15 -0300

    feat/folders architeture

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,12 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     'react/display-name': 'off',
     'react/prop-types': 'off',
-    'prettier/prettier': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   settings: {
     react: {
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
-import App from './App';
 import {name as appName} from './app.json';
+import App from './src/App';
 
 AppRegistry.registerComponent(appName, () => App);
