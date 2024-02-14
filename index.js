36839683dc88015586edd33485becb6f27916936
Kaua Librelato da Costa <kaualibrelatodacosta@gmail.com>
2024-02-14 16:38:20 -0300

    feat/add reflect-metadata

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
+import 'reflect-metadata';
 import {name as appName} from './app.json';
 import App from './src/App';
 
